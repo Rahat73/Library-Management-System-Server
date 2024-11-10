af2659d5046c4d4f60474753a0da0abc505f3f1d
Rahat73 <rahat.ashik.18@gmail.com>
2024-11-11 01:32:23 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import globalErrorHandler from "./app/middlewares/global-error-handler";
+import noRoutesFound from "./app/middlewares/no-routes-found-handler";
+import router from "./app/routes";
+
+const app: Application = express();
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "Library Management System Server..",
+  });
+});
+
+// app.use("/api", router);
+
+app.use(globalErrorHandler);
+
+app.use(noRoutesFound);
+
+export default app;
---
+++ src/app/routes/index.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.use("/books");
+
+export default router;
